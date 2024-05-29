2c62717105e263e483ce0e568fe60bfc13788615
spageektti <git@spageektti.cc>
2024-05-30 00:01:21 +0200

    add Dart example

---
+++ hello-world.dart
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, spageektti
+// This code is licensed under the MIT License.
+
+// Main function that is called when the program is run
+void main() {
+  // Print the text "Hello World!" to the console
+  print('Hello World!');
+}
