0f475d8914ba4169454af95a768fc9cdca82d9a5
spageektti <git@spageektti.cc>
2024-05-29 23:58:17 +0200

    add C++ example

---
+++ hello-world.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2024, spageektti
+// This code is licensed under the MIT License.
+
+#include <iostream> // Include the library necessary for printing to the console
+
+// The main function, which is executed when you run the program
+int main() {
+    std::cout << "Hello World!"; // Print "Hello World!" to standard output (the console)
+    return 0; // End the program with a return code of 0 (indicating successful completion)
+}
